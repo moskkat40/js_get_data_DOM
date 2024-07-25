f06f474c0f4687c92d6d282ff602f133e12ebfc0
Kateryna <moskkat40@gmail.com>
2024-07-25 21:06:10 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const populationOfCountries = document.querySelectorAll(".population");
+const inputTotal = document.querySelector(".total-population");
+const inputAvarage = document.querySelector(".average-population");
+const populationToNumber = [
+    ...populationOfCountries
+].map((currentNode)=>{
+    return currentNode.innerHTML.replaceAll(",", "");
+});
+const sum = populationToNumber.reduce((acc, number)=>{
+    return acc + +number;
+}, 0);
+const total = sum.toLocaleString();
+const avarage = (sum / populationToNumber.length).toLocaleString();
+inputTotal.innerHTML = total;
+inputAvarage.innerHTML = avarage;
+
+//# sourceMappingURL=index.f75de5e1.js.map
